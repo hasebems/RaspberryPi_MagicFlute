4add51b4c0d36ca417037d9d563c77b9fac4dc89
Hasebe Masahiko <JCA03205@nifty.com>
2014-07-19 10:33:55 +0900

    加速度センサ左詰め

--- raspi_hw.c
+++ raspi_hw.c
@@ -444,8 +444,8 @@ void initADXL345( void )
 {
 	//	Start Access
 	accessADXL345();
-	writeI2c(ACCEL_SNCR_PWR_CTRL,0x08);		//	Start Measurement
-	writeI2c(ACCEL_SNCR_PWR_CTRL,0x04);		//	Left Justified
+	writeI2c(ACCEL_SNCR_PWR_CTRL,0x08);			//	Start Measurement
+	writeI2c(ACCEL_SNCR_DATA_FORMAT,0x04);		//	Left Justified
 }
 //-------------------------------------------------------------------------
 void getAccel( unsigned short* value )
