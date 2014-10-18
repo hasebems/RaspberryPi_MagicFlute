9a8085e339a8a1bf9df5189b8f32583b7301e9eb
Hasebe Masahiko <JCA03205@nifty.com>
2014-10-18 18:02:51 +0900

    オプションスイッチ追加

--- raspi_main.c
+++ raspi_main.c
@@ -457,7 +457,7 @@ static int optionCommand(int morehelp, int argc, char *argv[])
 
 	while (1) {
 		int c;
-		if ((c = getopt_long(argc, argv, "hD:r:c:f:b:p:m:o:vnet:a", long_option, NULL)) < 0)
+		if ((c = getopt_long(argc, argv, "hD:r:c:f:b:p:m:o:vnet:al", long_option, NULL)) < 0)
 			break;
 		switch (c) {
 			case 'h':
