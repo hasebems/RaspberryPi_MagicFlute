216d4f6a8c2996b868d103f32f7ac92f91184e84
Hasebe Masahiko <JCA03205@nifty.com>
2014-10-20 23:48:28 +0900

    感度変更

--- raspi_hw.c
+++ raspi_hw.c
@@ -246,8 +246,8 @@ int getPressure( void )
 #define 	TCH_SNCR_EPROXTTH			0x59
 
 // Threshold defaults
-#define		E_THR_T      0x08	// Electrode touch threshold
-#define		E_THR_R      0x04	// Electrode release threshold
+#define		E_THR_T      0x02	// Electrode touch threshold
+#define		E_THR_R      0x01	// Electrode release threshold
 #define		PROX_THR_T   0x02	// Prox touch threshold
 #define		PROX_THR_R   0x02	// Prox release threshold
 
